8d2e427f47ad78a00e1ff5c17bdd0ddf4dbab333
kwmorris <k.w.morris@gmail.com>
2026-06-08 23:05:57 -0500

    Code clean-up: removed commented out code, and updated HOST URLs from 'localhost' to the OSU server.

--- src/pages/AssignmentsPage.jsx
+++ src/pages/AssignmentsPage.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
-// import assignmentsData from '../data/assignments';
+import { useState } from 'react';
 import AssignmentsTable from '../components/AssignmentsTable';
 import axios from 'axios';
 
-// const HOST = 'classwork.engr.oregonstate.edu';
-const HOST = 'localhost';
+const HOST = 'classwork.engr.oregonstate.edu';
 const PORT = 13331;
 
 function AssignmentsPage() {
@@ -18,12 +16,10 @@ function AssignmentsPage() {
         points: ''
     };
 
-    useEffect(() => {
-        console.log("Refresh Table")    
-        refreshTable();
-    });
+    window.onload = (event) => {refreshTable()}
 
     function refreshTable() {
+        console.log("Refresh table")
         axios.get(`http://${HOST}:${PORT}/assignments`)
             .then(res => setAssignments(res.data))
             .catch(err => console.log(err));
@@ -39,11 +35,10 @@ function AssignmentsPage() {
         e.preventDefault();
         let url = `http://${HOST}:${PORT}/assignments`
         axios.post(url, newAssignment)
-            .catch(err => console.log(err))
-            .then(response => {
-                console.log("Post response")
+            .then(res => {
                 refreshTable()
-            });
+            })
+            .catch(err => console.log(err));
         setNewAssignment(blankAssignment);
     }
 
@@ -62,44 +57,6 @@ function AssignmentsPage() {
                 <input name="points" placeholder="Points" value={newAssignment.points} onChange={handleNewChange} />
                 <button type="submit">Add Assignment</button>
             </form>
-
-            {/* <hr />
-
-            <h3>Update Assignment</h3>
-            <form onSubmit={updateExistingAssignment}>
-                <select value={selectedAssignmentID} onChange={selectAssignment}>
-                    <option value="">Select an assignment</option>
-                    {assignments.map((assignment) => (
-                        <option key={assignment.assignmentID} value={assignment.assignmentID}>
-                            {assignment.name}
-                        </option>
-                    ))}
-                </select>
-
-                <input name="assignmentID" placeholder="Assignment ID" value={updateAssignment.assignmentID} onChange={handleUpdateChange} />
-                <input name="name" placeholder="Assignment Name" value={updateAssignment.name} onChange={handleUpdateChange} />
-                <input name="description" placeholder="Description" value={updateAssignment.description} onChange={handleUpdateChange} />
-                <input name="dueDate" placeholder="Due Date" value={updateAssignment.dueDate} onChange={handleUpdateChange} />
-                <input name="points" placeholder="Points" value={updateAssignment.points} onChange={handleUpdateChange} />
-
-                <button type="submit">Update Assignment</button>
-            </form>
-
-            <hr />
-
-            <h3>Delete Assignment</h3>
-            <form onSubmit={deleteAssignment}>
-                <select value={deleteAssignmentID} onChange={(e) => setDeleteAssignmentID(e.target.value)}>
-                    <option value="">Select an assignment</option>
-                    {assignments.map((assignment) => (
-                        <option key={assignment.assignmentID} value={assignment.assignmentID}>
-                            {assignment.name}
-                        </option>
-                    ))}
-                </select>
-
-                <button type="submit">Delete Assignment</button>
-            </form> */}
         </main>
     );
 }
